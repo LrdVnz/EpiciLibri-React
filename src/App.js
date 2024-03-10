a92113aa0cce85c526f92aae9a07cfb1001318e9
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-03-10 17:29:29 +0100

    Books cards working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import BasicFooter from "./BasicFooter";
 import BasicNav from "./BasicNav";
+import Main from "./Main";
+import Books from "./books/fantasy.json";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 const App = () => {
-    return ( 
-        <main>
-           <BasicNav /> 
-           <BasicFooter />
-        </main>
-     );
-}
- 
+  return (
+    <main>
+      <BasicNav />
+      <Container fluid>
+        <Row className="g-2">
+          <Main books={Books} />
+        </Row>
+      </Container>
+      <BasicFooter />
+    </main>
+  );
+};
+
 export default App;
