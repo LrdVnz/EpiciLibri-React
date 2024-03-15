f44a8095ed5b270e1e0128799fa3b74faa1638a3
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-03-15 08:34:07 +0100

    Card border change onclick done

--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -1,21 +1,26 @@
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import { useState } from "react";
+import "./SingleBook.css"
 
-const SingleBook = ({ book , key }) => {
-    return ( 
-        <Col md={2} key={key}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={book.img} />
-          <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-            <Card.Text>
-              <span> {book.category} </span>
-              <span> € {book.price} </span>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-     );
-}
- 
+const SingleBook = ({ book, key }) => {
+
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <Col md={2} key={key}>
+      <Card style={{ width: "18rem" }} className = { selected ? "custom-bg-danger" : "" }>
+        <Card.Img variant="top" src={book.img} onClick={() => ( selected ? setSelected(false) : setSelected(true) )} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text>
+            <span> {book.category} </span>
+            <span> € {book.price} </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
 export default SingleBook;
