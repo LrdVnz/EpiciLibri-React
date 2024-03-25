e88e004a3bb702839e0bd1936843a7c669584a09
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-03-25 22:44:12 +0100

    app.js refactoring

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-/// Import di react
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContextProvider";
+import { BasicFooter, BasicNav, Main, Container, fantasy, historyBooks, horror, romance, scifi, } from "./components/Importer";
 
-/// Import di bootstrap
-import BasicFooter from "./components/BasicFooter";
-import BasicNav from "./components/BasicNav";
-import Main from "./components/Main";
-import Container from "react-bootstrap/Container";
-
-/// Import per i libri in json.
-import fantasy from "./books/fantasy.json";
-import historyBooks from "./books/history.json";
-import horror from "./books/horror.json";
-import romance from "./books/romance.json";
-import scifi from "./books/scifi.json";
-
+const categories = {
+  fantasy: fantasy,
+  historyBooks: historyBooks,
+  horror: horror,
+  romance: romance,
+  scifi: scifi,
+};
 const App = () => {
   const [searchInput, setSearchInput] = useState("");
 
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const categories = {
-    fantasy: fantasy,
-    historyBooks: historyBooks,
-    horror: horror,
-    romance: romance,
-    scifi: scifi,
-  };
-
+  const { theme } = useContext(ThemeContext);
+  
   const [books, setBooks] = useState(fantasy);
   const [loading, setLoading] = useState(false);
+
 
   let bookList = books;
 
@@ -47,19 +34,17 @@ const App = () => {
     }, 1500);
   };
 
+  const loadingMsg = 
+  <p className={theme === "dark" ? "text-light" : "text-dark"}>
+    loading..
+  </p>
+
   return (
     <main>
-      <BasicNav
-        handleDropdown={handleDropdown}
-        setSearchInput={setSearchInput}
-        searchInput={searchInput}
-      />
+      <BasicNav handleDropdown={handleDropdown} setSearchInput={setSearchInput} 
+      searchInput={searchInput} />
       <Container className={`bg-${theme}`} fluid>
-        {loading && (
-          <p className={theme === "dark" ? "text-light" : "text-dark"}>
-            loading..
-          </p>
-        )}
+        {loading && loadingMsg}
         <Main books={bookList} />
       </Container>
       <BasicFooter />
@@ -68,27 +53,3 @@ const App = () => {
 };
 
 export default App;
-
-/*
-import { createContext } from "react";
-import { useState } from "react";
-
-export const ThemeContext = createContext(null);
-
-const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dar k");
-
-  const value = {
-    theme,
-    setTheme,
-  };
-
-  return (
-    <ThemeContext.Provider value={value}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
-
-export default ThemeContextProvider;
-*/
---
+++ src/components/Importer.js
@@ -0,0 +1,24 @@
+// Per App.js
+import BasicFooter from "./BasicFooter";
+import BasicNav from "./BasicNav";
+import Main from "./Main";
+import Container from "react-bootstrap/Container";
+
+/// Import per i libri in json.
+import fantasy from "../books/fantasy.json";
+import historyBooks from "../books/history.json";
+import horror from "../books/horror.json";
+import romance from "../books/romance.json";
+import scifi from "../books/scifi.json";
+
+export {
+    BasicFooter,
+    BasicNav,
+    Main,
+    Container,
+    fantasy,
+    historyBooks,
+    horror,
+    romance,
+    scifi,
+  };
