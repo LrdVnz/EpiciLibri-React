86e47a643d7f7cff55bcc6de73686d03895b61e6
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-03-27 20:39:33 +0100

    basic bookDetails done

--- src/components/books/BookDetails.js
+++ src/components/books/BookDetails.js
@@ -1,23 +1,35 @@
 import { useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { QueryContext } from "../../contexts/QueryContext";
+import { Card, Col, Container } from "react-bootstrap";
 
 const BookDetails = () => {
-    
-    const { query } = useContext(QueryContext)
+  const { query } = useContext(QueryContext);
+  const { asin } = useParams();
 
-    useEffect(( ) => {
-        console.log(query)
-    }, [query])
+  let book = query.find((book) => {
+    if (book.asin === asin) {
+      return book;
+    }
+  });
 
-    const { asin } = useParams();
-    
-    return ( 
-       <>
-       
-       
-       </>
-     );
-}
- 
+  return (
+    <Container>
+      <Col md={6}>
+        <Card>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              <span> {book.category} </span>
+              <span> € {book.price} </span>
+              Asin: {book.asin}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Container>
+  );
+};
+
 export default BookDetails;
