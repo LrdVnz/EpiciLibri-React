587c199329473a2902340872484131ec063ce00e
Vincenzo <58110589+LrdVnz@users.noreply.github.com>
2024-04-01 17:09:42 +0200

    basic layout for details page

--- src/components/books/BookDetails.js
+++ src/components/books/BookDetails.js
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContextProvider";
 import { QueryContext } from "../../contexts/QueryContext";
-import { Card, Col, Container } from "react-bootstrap";
+import { Row, Card, Col, Container } from "react-bootstrap";
 import BasicNav from "../BasicNav";
 import BasicFooter from "../BasicFooter";
 
@@ -20,25 +20,32 @@ const BookDetails = () => {
 
   return (
     <>
-     <BasicNav
-        showSearch={false}
-      />
-    <Container className={`bg-${theme}`} fluid>
-      <Col md={6}>
-        <Card>
-          <Card.Img variant="top" src={book.img} />
-          <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-            <Card.Text>
-              <span> {book.category} </span>
-              <span> € {book.price} </span>
-              Asin: {book.asin}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Container>
-    <BasicFooter />
+      <BasicNav showSearch={false} />
+      <Container className={`bg-${theme}`} fluid style={{ height: "100vh" }}>
+        <Row>
+          <Col md={6}>
+            <Row>
+              <Col className="offset-md-3" md={6}>
+                <Card>
+                  <Card.Img variant="top" src={book.img} />
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col md={6} style={{ color: theme === "dark" ? "white" : "black" }}>
+            <Row>
+              <Col className="offset-md-2" md={8}>
+                <h1>{book.title}</h1>
+                <h3> {book.category} </h3>
+                <h3> € {book.price} </h3>
+                Asin: {book.asin}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      <BasicFooter />
     </>
   );
 };
